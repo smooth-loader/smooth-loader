46fa8f466812e3f7588c2ed94546ac3c390759af
SerhiiCho <serhiicho@protonmail.com>
2022-12-04 10:43:29 +0200

    Compiled for prod

---
+++ lib/js/imageLoader.d.ts
@@ -0,0 +1,7 @@
+import { LazyImage } from './types';
+/**
+ * If image has tag IMG then set the src attribute to img url,
+ * otherwise set the background of the element to given image url
+ */
+export declare function loadImage(lazyImage: LazyImage): void;
+//# sourceMappingURL=imageLoader.d.ts.map
