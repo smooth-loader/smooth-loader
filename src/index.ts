2d1e436111dc1c865052225b16ccfd745c716589
SerhiiCho <serhiicho@protonmail.com>
2025-03-28 21:40:38 +0200

    Add prettierrc

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,36 @@ import observerCreator from './observerCreator'
 import { Config, ImageElement, InputTypes } from './types'
 
 export default (selector?: InputTypes, config?: Config): void => {
-    const images = getImagesElements(selector)
+	const images = getImagesElements(selector)
 
-    if (!images) {
-        return
-    }
+	if (!images) {
+		return
+	}
 
-    const configurations = config || {
-        root: null,
-        threshold: 0,
-    }
+	const configurations = config || {
+		root: null,
+		threshold: 0,
+	}
 
-    return observerCreator(images, configurations)
+	return observerCreator(images, configurations)
 }
 
 function getImagesElements(selector?: InputTypes): ImageElement[] {
-    if (!selector) {
-        const elements = document.querySelectorAll<ImageElement>('.smooth-loader[data-src]')
-        return Array.from(elements)
-    }
+	if (!selector) {
+		const elements = document.querySelectorAll<ImageElement>(
+			'.smooth-loader[data-src]',
+		)
+		return Array.from(elements)
+	}
 
-    if (typeof selector === 'string') {
-        const elements = document.querySelectorAll<ImageElement>(selector)
-        return Array.from(elements)
-    }
+	if (typeof selector === 'string') {
+		const elements = document.querySelectorAll<ImageElement>(selector)
+		return Array.from(elements)
+	}
 
-    if (selector instanceof NodeList) {
-        return Array.from(selector)
-    }
+	if (selector instanceof NodeList) {
+		return Array.from(selector)
+	}
 
-    return selector
+	return selector
 }
